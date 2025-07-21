dacddbed09ddd76639f652755a611b5e5fbfafd4
Мария Куимова <mariakuimova@Noutbuk-Maria.local>
2025-07-21 20:40:32 +0300

    Первоначальный коммит

---
+++ serviceworker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "ntagil-pwa-v1";
+const urlsToCache = [
+  "index.html",
+  "dostoprim.html",
+  "factories.html",
+  "culture.html",
+  "contact.html",
+  "todo.html",
+  "css/style.css",
+  "images/vid_night.jpeg",
+  "images/lisya_gora.jpeg",
+  "images/museum.jpeg",
+  "images/pond.jpeg",
+  "images/factory.jpeg",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
